9e1ab343a2924fd466cc64026337e34847d8a992
Jinan Lakan <lakanj25@gmail.com>
2023-08-07 02:25:28 +0200

    Create side bar close btn

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,14 @@
 const sideLinks = Array.from(document.querySelectorAll('.side-link a'))
 const sidebar = document.getElementById('side-bar')
 const navBtn = document.getElementById('nav-btn')
+const closeBtn = document.getElementById('close-btn')
 console.log(sideLinks)
 
 navBtn.addEventListener('click', (e)=>{
+    sidebar.classList.toggle('show-side')
+})
+
+closeBtn.addEventListener('click', (e)=>{
     sidebar.classList.toggle('show-side')
 })
 sideLinks.forEach((link)=>{
